0b156d0b0b3f9fd37e301345ff67a231224003f6
Anders Karlsen <karlsenanders@hotmail.com>
2012-02-26 13:53:40 +0100

    Finds one solution

--- src/clojureagent.clj
+++
@@ -1,9 +0,0 @@
-(ns clojureagent)
-
-(defn counteragent [x]
-  (let [counter (agent 0)]
-    (cond 
-      (= @counter 1) x
-      (<
-      
-
--- src/soduko.clj
+++ src/soduko.clj
@@ -59,8 +59,7 @@
 (with-test
   (defn legal-values [board size n]
     "Finds the legal values for a cell"
-    (let [illegal (set (illegal-values board size n))]
-    
+    (let [illegal (set (illegal-values board size n))]    
     (filter #(not (contains? illegal %)) (range 1 (+ size 1)))
     )
     )
@@ -68,46 +67,22 @@
   (is (= [1 2 3 7 8] (sort (legal-values board 9 0))))
   )
 
-(with-test
-  (defn replace-item [board n new-value]
-    "Replaces an item with a new value"
-    (concat (take n board) [new-value] (drop (+ n 1) board)) 
-    )
-  (is (= [1 8 3 4] (replace-item [1 2 3 4] 1 8)))
-  )
-
-(with-test
-  (defn finished? [board]
-    "A finished board contains no zeroes"
-    (not (contains? (set board) 0))
-    )
-  (is (not (finished? board)))
-  (is (finished? [1 2 3]))
-  )
-
-(defn solved-it [board]
-  (println "Solved " board)
-  board
-  )
-
-(def finished (atom []))
-
 (with-test 
   (defn solve [board size]
     "Solves it"
-    (cond
-      (not (empty? @finished)) []
-      (.contains board 0)
-      (let [index (.indexOf board 0)]
-        (let [legals (legal-values board size index)]            
-        (flatten (map #(solve (assoc board index %) size) (legal-values board size index)))
-        )
-      )
-      :else
-      (reset! finished board)
+    (let [solve-for-space (fn ! [board values size solution]
+                            (if (or (not (empty? solution)) (empty? values)) 
+                              solution
+                              (! board (rest values) size (solve (assoc board (.indexOf board 0) (first values)) size))
+                            )
+                          )
+          ]
+    (if     
+      (not (.contains board 0)) board      
+      (solve-for-space board (legal-values board size (.indexOf board 0)) size [])       
     )
-   )
-  (is (= [1 2 2 1] (solve [0 2 2 0] 2 999)))
+   ))
+  (is (= [1 2 2 1] (solve [0 2 2 0] 2)))
 )
 
-(solve default-problem 9 999)
+(solve default-problem 9)
